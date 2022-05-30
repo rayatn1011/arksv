501867d111851a7f5b674c2dc54deaf4dab00481
rayatn1011 <80741494+rayatn1011@users.noreply.github.com>
2022-05-30 11:45:49 +0800

    feat:新增換頁時捲動軸會自動至頂功能

--- src/router/index.js
+++ src/router/index.js
@@ -74,6 +74,9 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes: routes,
+  scrollBehavior() {
+    return { top: 0 }
+  },
 })
 
 router.beforeEach(async (to) => {
